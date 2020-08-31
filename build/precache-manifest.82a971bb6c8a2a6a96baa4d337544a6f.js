b9579d48f382dee99a2671624954b13997098aec
Laimis <62464661+Laimik@users.noreply.github.com>
2020-08-31 20:13:01 +0300

    Add files via upload

---
+++ build/precache-manifest.82a971bb6c8a2a6a96baa4d337544a6f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fecbaa006585831eda43598d8c2220e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "193fa04d7da4d7d78f40",
+    "url": "/static/css/main.4a163760.chunk.css"
+  },
+  {
+    "revision": "9a65224c3442f481d6d9",
+    "url": "/static/js/2.0db896fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0db896fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "193fa04d7da4d7d78f40",
+    "url": "/static/js/main.ddffe66d.chunk.js"
+  },
+  {
+    "revision": "f6c41c31908d27e9d5bf",
+    "url": "/static/js/runtime-main.7fb9a7cf.js"
+  },
+  {
+    "revision": "a25947896caea6313f83c09784fb10d6",
+    "url": "/static/media/app.a2594789.jpg"
+  },
+  {
+    "revision": "f2b50dc05665affdb626e560dd12ffa6",
+    "url": "/static/media/cold.f2b50dc0.png"
+  },
+  {
+    "revision": "7fdd23c2c89b5ea25fced006b4d53f9b",
+    "url": "/static/media/hot.7fdd23c2.jpg"
+  }
+]);
